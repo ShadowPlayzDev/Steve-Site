5996137786ab6c47e26f1add2c1315682b3a081e
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-31 09:15:37 -0500

    Create projects.js

---
+++ public/projects.js
@@ -0,0 +1,14 @@
+
+const projects = [
+  {
+    "title": "Tools Website",
+    "description": "A modern website with a bunch of tools.",
+    "image": "./assets/projects/project_tools.webp",
+    "technologies": ["HTML", "CSS", "JavaScript", "Tailwind CSS"],
+    "liveLink": "https://steveow.vercel.app",
+    "githubLink": "https://github.com/ShadowPlayzDev/Steve-Backend"
+  }
+  // Add more projects here
+];
+
+export default projects;
