dc82bd839ea1b9c3fc67dbd9bd52c2ee09059217
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-31 16:56:34 -0500

    Update contact-config.js

--- public/contact-config.js
+++ public/contact-config.js
@@ -1,10 +1,10 @@
 window.contactConfig = {
-    theme: "light", // 'light', 'dark', or 'app'
+    theme: "app", // 'light', 'dark', or 'app'
     title: "Get in Touch",
     message: "I'm always open to a new conversation. Find me anywhere below. I will get to your message as soon as I can.",
     email: "shadowplayzdev@gmail.com",
     socialIcons: [
-        { platform: "discord", handle: "shadowplayzdev", icon: "https://cdn.discordapp.com/attachments/1247012674997092403/1247012693892749322/discord-logo-white-min.png?ex=665e77b4&is=665d2634&hm=4a781d4fb02891d4e0e56e0766324a35cf91d7c0411ed026117ef4147770e176&" },
+        { platform: "discord", handle: "shadowplayzdev", icon: "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/66e278299a53f5bf88615e90_Symbol.svg" },
         { platform: "github", handle: "ShadowPlayzDev", icon: "https://img.icons8.com/ios-filled/50/FFFFFF/github.png"},
     ]
 };
