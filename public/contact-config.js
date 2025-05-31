6e3f20024c0b225e1d9556c175d5ccabe8f21b7b
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-31 17:09:10 -0500

    Update contact-config.js

--- public/contact-config.js
+++ public/contact-config.js
@@ -5,6 +5,6 @@ window.contactConfig = {
     email: "shadowplayzdev@gmail.com",
     socialIcons: [
         { platform: "discord", handle: "shadowplayzdev", icon: "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/66e278299a53f5bf88615e90_Symbol.svg" },
-        { platform: "github", handle: "ShadowPlayzDev", icon: "https://img.icons8.com/ios-filled/50/FFFFFF/github.png"},
+        { platform: "github", handle: "ShadowPlayzDev", icon: "https://img.icons8.com/ios-filled/50/FFFFFF/github.png"}
     ]
 };
