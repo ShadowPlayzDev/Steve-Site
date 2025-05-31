dd1b7deee83b0664b125b3ed1a50bc76780d549c
ShadowPlayz <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-05-31 09:19:22 -0500

    Create tesstimonials.js

---
+++ public/tesstimonials.js
@@ -0,0 +1,10 @@
+// Variables
+// Quote, Author, Company.
+const testimonials = [
+  {
+    "quote": "No Testimonials yet. Be the first one!",
+    "author": "Steve",
+  }
+];
+
+export default testimonials;
